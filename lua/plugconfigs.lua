1bb603212f188ae8e69089939ed2c102c1be2689
menghuiguli <fujia0@qq.com>
2022-03-07 13:29:59 +0800

    mini.complete

--- lua/plugconfigs.lua
+++ lua/plugconfigs.lua
@@ -135,3 +135,50 @@ cmp.setup {
     end
   }
 }
+local completion = require "mini.completion"
+completion.setup{
+  -- Delay (debounce type, in ms) between certain Neovim event and action.
+  -- This can be used to (virtually) disable certain automatic actions by
+  -- setting very high delay time (like 10^7).
+  delay = { completion = 100, info = 100, signature = 50 },
+
+  -- Maximum dimensions of floating windows for certain actions. Action
+  -- entry should be a table with 'height' and 'width' fields.
+  window_dimensions = {
+    info = { height = 25, width = 80 },
+    signature = { height = 25, width = 80 },
+  },
+
+  -- Way of how module does LSP completion
+  lsp_completion = {
+    -- `source_func` should be one of 'completefunc' or 'omnifunc'.
+    source_func = 'completefunc',
+
+    -- `auto_setup` should be boolean indicating if LSP completion is set up
+    -- on every `BufEnter` event.
+    auto_setup = true,
+
+    -- `process_items` should be a function which takes LSP
+    -- 'textDocument/completion' response items and word to complete. Its
+    -- output should be a table of the same nature as input items. The most
+    -- common use-cases are custom filtering and sorting. You can use
+    -- default `process_items` as `MiniCompletion.default_process_items()`.
+    -- process_items = MiniCompletion.default_process_items(),--<function: filters out snippets; sorts by LSP specs>,
+  },
+
+  -- Fallback action. It will always be run in Insert mode. To use Neovim's
+  -- built-in completion (see `:h ins-completion`), supply its mapping as
+  -- string. Example: to use 'whole lines' completion, supply '<C-x><C-l>'.
+  fallback_action = '<C-n>',--<function: like `<C-n>` completion>,
+
+  -- Module mappings. Use `''` (empty string) to disable one. Some of them
+  -- might conflict with system mappings.
+  mappings = {
+    force_twostep = '<C-Space>', -- Force two-step completion
+    force_fallback = '<A-Space>', -- Force fallback completion
+  },
+
+  -- Whether to set Vim's settings for better experience (modifies
+  -- `shortmess` and `completeopt`)
+  set_vim_settings = true,
+}
--- lua/plugs.lua
+++ lua/plugs.lua
@@ -2,6 +2,7 @@ local use = require('packer').use
 require('packer').startup(function()
 	use 'wbthomason/packer.nvim' -- Package manager
 	use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+	use { 'echasnovski/mini.nvim', branch = 'stable' }
 	use {
 		'nvim-lualine/lualine.nvim',
 		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
