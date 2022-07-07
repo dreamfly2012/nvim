11994bb51988de7dfeefdfb7b8c588574bcb3dfd
menghuiguli <fujia0@qq.com>
2022-07-07 21:23:02 +0800

    nvim-autopairs

--- lua/plugs.lua
+++ lua/plugs.lua
@@ -22,10 +22,6 @@ require('packer').startup(function()
         'hrsh7th/vim-vsnip'
     }
     use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
-    use {
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    }
 
     use {
         'ekickx/clipboard-image.nvim'
