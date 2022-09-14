08de0d2fdcc155028baf3eb5612f41e9bc27c692
menghuiguli <fujia0@qq.com>
2022-09-15 06:52:08 +0800

    neotest keybinds

--- lua/plugconfigs.lua
+++ lua/plugconfigs.lua
@@ -73,6 +73,19 @@ require('telescope').setup{
 
 local neotest = require 'neotest'
 neotest.setup({
+    icons = {
+      expanded = "",
+      child_prefix = "",
+      child_indent = "",
+      final_child_prefix = "",
+      non_collapsible = "",
+      collapsed = "",
+
+      passed = "",
+      running = "",
+      failed = "",
+      unknown = ""
+    },
     adapters = {
         require('neotest-phpunit')({
             phpunit_cmd = function()
