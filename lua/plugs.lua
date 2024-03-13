33df7b7bc24b68d30a1ad5a057e3faf044603b69
menghuiguli <fujia0@qq.com>
2024-03-13 21:39:17 +0800

    which-key

--- lua/plugs.lua
+++ lua/plugs.lua
@@ -1,190 +1,201 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "git@github.com:folke/lazy.nvim",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"git@github.com:folke/lazy.nvim",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    "windwp/nvim-autopairs",
-    "Mofiqul/dracula.nvim",
-    "folke/tokyonight.nvim",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    'numToStr/Comment.nvim',
-    "folke/neodev.nvim",
-    'liuchengxu/vista.vim',
-    "lervag/vimtex",
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'jwalton512/vim-blade',
-    'nvim-tree/nvim-web-devicons',
-    'mhartington/formatter.nvim',
-    'MunifTanjim/prettier.nvim',
-    "akinsho/toggleterm.nvim",
-    "ojroques/vim-oscyank",
-    "nvim-neotest/neotest",
-    "nvim-lua/plenary.nvim",
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    "antoinemadec/FixCursorHold.nvim",
-    'olimorris/neotest-phpunit',
-    --theme
-    'dracula/vim',
-    'nvim-telescope/telescope.nvim',
-    'nvim-lua/popup.nvim',
-    -- code runner
-    { 'asamonik/nvim-screenshot', build = 'go build' },
-    'segeljakt/vim-silicon',
-
-    'ekickx/clipboard-image.nvim',
-
-    {
-        'othree/html5.vim'
-    },
-    {
-        'voldikss/vim-translator'
-    },
-    {
-        'pangloss/vim-javascript'
-    },
-    {
-        'evanleck/vim-svelte'
-    },
-    {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    },
-    {
-        'hotoo/pangu.vim'
-    },
-    -- markdown
-    {
-        'godlygeek/tabular',
-        'preservim/vim-markdown',
-    },
-    {
-        'folke/zen-mode.nvim',
-        config = function()
-            require("zen-mode").setup {
-                window = {
-                    width = 90,
-                    options = {
-                        -- signcolumn = "no", -- disable signcolumn
-                        number = false,         -- disable number column
-                        relativenumber = false, -- disable relative numbers
-                        cursorline = false,     -- disable cursorline
-                        cursorcolumn = false,   -- disable cursor column
-                        -- foldcolumn = "0", -- disable fold column
-                        -- list = false, -- disable whitespace characters
-                    }
-                }
-            }
-        end
-    },
-    {
-        "folke/twilight.nvim",
-        config = function()
-            require("twilight").setup {
-            }
-        end
-    },
-    {
-        "mfussenegger/nvim-dap",
-        config = function()
-            require("dap.nvim-dap")
-        end
-    },
-    -- 为代码调试提供内联文本
-    {
-        "theHamsta/nvim-dap-virtual-text",
-        requires = {
-            "mfussenegger/nvim-dap"
-        },
-        config = function()
-            require("dap.nvim-dap-virtual-text")
-        end
-    },
-    -- 为代码调试提供 UI 界面
-    {
-        "rcarriga/nvim-dap-ui",
-        requires = {
-            "mfussenegger/nvim-dap"
-        },
-        config = function()
-            require("dap.nvim-dap-ui")
-        end
-    },
-    'mattn/emmet-vim',
-    'junegunn/fzf',
-    'junegunn/fzf.vim',
-    'windwp/nvim-ts-autotag',
-    "ellisonleao/glow.nvim",
-    {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons',
-        },
-        config = function() require 'nvim-tree'.setup {} end
-    },
-    { 'tami5/lspsaga.nvim' },
-    {
-        'crispgm/nvim-go',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-lua/popup.nvim'
-        },
-        config = function() require 'go'.setup {} end
-    },
-    { 'nvim-treesitter/nvim-treesitter' },
-    { 'nvim-lua/lsp-status.nvim' },
-    "SDGLBL/nvim-sync.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("nvim-sync").setup {
-        -- sync_exe_filename default name of the sync executable
-        sync_exe_filename = ".sync",
-        -- enable_paths
-        enable_paths = {},
-        -- sync_exe_path path to the sync executable
-        -- if not set, it will be searched in the project root path by default
-        -- if not found, it will be searched in the system path
-        -- if not found, it will return an error
-        sync_exe_path = nil,
-        -- Methods of detecting the root directory.
-        -- if one is not detected, the other is used as fallback. You can also delete or rearangne the detection methods.
-        detection_methods = { "pattern", "lsp" },
-        -- All the patterns used to detect root dir, when **"pattern"** is in
-        -- detection_methods
-        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "package.json" },
-        -- Table of lsp clients to ignore by name
-        -- eg: { "efm", ... }
-        ignore_lsp = {},
-      }
-    end,
+	"windwp/nvim-autopairs",
+	"Mofiqul/dracula.nvim",
+	"folke/tokyonight.nvim",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	'numToStr/Comment.nvim',
+	"folke/neodev.nvim",
+	'liuchengxu/vista.vim',
+	"lervag/vimtex",
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-path',
+	'hrsh7th/cmp-cmdline',
+	'hrsh7th/nvim-cmp',
+	'L3MON4D3/LuaSnip',
+	'saadparwaiz1/cmp_luasnip',
+	'jwalton512/vim-blade',
+	'nvim-tree/nvim-web-devicons',
+	'mhartington/formatter.nvim',
+	'MunifTanjim/prettier.nvim',
+	"akinsho/toggleterm.nvim",
+	"ojroques/vim-oscyank",
+	"nvim-neotest/neotest",
+	"nvim-lua/plenary.nvim",
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"antoinemadec/FixCursorHold.nvim",
+	'olimorris/neotest-phpunit',
+	--theme
+	'dracula/vim',
+	'nvim-telescope/telescope.nvim',
+	'nvim-lua/popup.nvim',
+	-- code runner
+	{ 'asamonik/nvim-screenshot',        build = 'go build' },
+	'segeljakt/vim-silicon',
+	'ekickx/clipboard-image.nvim',
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+	{
+		'othree/html5.vim'
+	},
+	{
+		'voldikss/vim-translator'
+	},
+	{
+		'pangloss/vim-javascript'
+	},
+	{
+		'evanleck/vim-svelte'
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	},
+	{
+		'hotoo/pangu.vim'
+	},
+	-- markdown
+	{
+		'godlygeek/tabular',
+		'preservim/vim-markdown',
+	},
+	{
+		'folke/zen-mode.nvim',
+		config = function()
+			require("zen-mode").setup {
+				window = {
+					width = 90,
+					options = {
+						-- signcolumn = "no", -- disable signcolumn
+						number = false, -- disable number column
+						relativenumber = false, -- disable relative numbers
+						cursorline = false, -- disable cursorline
+						cursorcolumn = false, -- disable cursor column
+						-- foldcolumn = "0", -- disable fold column
+						-- list = false, -- disable whitespace characters
+					}
+				}
+			}
+		end
+	},
+	{
+		"folke/twilight.nvim",
+		config = function()
+			require("twilight").setup {
+			}
+		end
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			require("dap.nvim-dap")
+		end
+	},
+	-- 为代码调试提供内联文本
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		requires = {
+			"mfussenegger/nvim-dap"
+		},
+		config = function()
+			require("dap.nvim-dap-virtual-text")
+		end
+	},
+	-- 为代码调试提供 UI 界面
+	{
+		"rcarriga/nvim-dap-ui",
+		requires = {
+			"mfussenegger/nvim-dap"
+		},
+		config = function()
+			require("dap.nvim-dap-ui")
+		end
+	},
+	'mattn/emmet-vim',
+	'junegunn/fzf',
+	'junegunn/fzf.vim',
+	'windwp/nvim-ts-autotag',
+	"ellisonleao/glow.nvim",
+	{
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons',
+		},
+		config = function() require 'nvim-tree'.setup {} end
+	},
+	{ 'tami5/lspsaga.nvim' },
+	{
+		'crispgm/nvim-go',
+		requires = {
+			'nvim-lua/plenary.nvim',
+			'nvim-lua/popup.nvim'
+		},
+		config = function() require 'go'.setup {} end
+	},
+	{ 'nvim-treesitter/nvim-treesitter' },
+	{ 'nvim-lua/lsp-status.nvim' },
+	"SDGLBL/nvim-sync.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("nvim-sync").setup {
+			-- sync_exe_filename default name of the sync executable
+			sync_exe_filename = ".sync",
+			-- enable_paths
+			enable_paths = {},
+			-- sync_exe_path path to the sync executable
+			-- if not set, it will be searched in the project root path by default
+			-- if not found, it will be searched in the system path
+			-- if not found, it will return an error
+			sync_exe_path = nil,
+			-- Methods of detecting the root directory.
+			-- if one is not detected, the other is used as fallback. You can also delete or rearangne the detection methods.
+			detection_methods = { "pattern", "lsp" },
+			-- All the patterns used to detect root dir, when **"pattern"** is in
+			-- detection_methods
+			patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "package.json" },
+			-- Table of lsp clients to ignore by name
+			-- eg: { "efm", ... }
+			ignore_lsp = {},
+		}
+	end,
 }, {
-    git = {
-        -- defaults for the `Lazy log` command
-        -- log = { "-10" }, -- show the last 10 commits
-        log = { "-8" }, -- show commits from the last 3 days
-        timeout = 120, -- kill processes that take more than 2 minutes
-        url_format = "git@github.com:%s.git",
-        -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
-        -- then set the below to false. This should work, but is NOT supported and will
-        -- increase downloads a lot.
-        filter = true,
-    },
+	git = {
+		-- defaults for the `Lazy log` command
+		-- log = { "-10" }, -- show the last 10 commits
+		log = { "-8" }, -- show commits from the last 3 days
+		timeout = 120, -- kill processes that take more than 2 minutes
+		url_format = "git@github.com:%s.git",
+		-- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
+		-- then set the below to false. This should work, but is NOT supported and will
+		-- increase downloads a lot.
+		filter = true,
+	},
 })
