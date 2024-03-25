842637ab078fca4cb724443f02d31bcf9709c13c
menghuiguli <fujia0@qq.com>
2024-03-25 14:36:03 +0800

    remove which-key

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -17,10 +17,10 @@ map('n', 'w-', ' :resize -3<cr>', {})
 map('n', 'w,', ":vert resize -3<cr>", {})
 map('n', 'w.', ":vert resize +3<cr>", {})
 
--- map('n', 'ff', "<cmd>Telescope find_files<cr>", {})
--- map('n', 'fg', "<cmd>Telescope live_grep<cr>", {})
--- map('n', 'fb', "<cmd>Telescope buffers<cr>", {})
--- map('n', 'fh', "<cmd>Telescope help_tags<cr>", {})
+map('n', 'ff', "<cmd>Telescope find_files<cr>", {})
+map('n', 'fg', "<cmd>Telescope live_grep<cr>", {})
+map('n', 'fb', "<cmd>Telescope buffers<cr>", {})
+map('n', 'fh', "<cmd>Telescope help_tags<cr>", {})
 
 -- neotest mappings
 map('n', 'tm', "<cmd>lua require('neotest').run.run() <cr>", {})
@@ -32,16 +32,3 @@ map('n', 'tdh', "<cmd>lua require('neotest').diagnostic.hide() <cr>", {})
 map('v', "yy", "<cmd>OSCYankVisual<cr>", { noremap = true })
 map('n', "<C-a>", "ggVG", { noremap = true })
 
-local wk = require("which-key")
-wk.register({
-    ["<leader>"] = {
-        f = {
-            name = "+file",
-            f = { "<cmd>Telescope find_files<cr>", "Find File" },
-            r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-            g = { "<cmd>Telescope live_grep<cr>", "Find String" },
-            b = { "<cmd>Telescope buffers<cr>", "Find From buffers" },
-            n = { "<cmd>enew<cr>", "New File" },
-        },
-    },
-})
